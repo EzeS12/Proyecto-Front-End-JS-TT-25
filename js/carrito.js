53c7e80080adb7b397165b85a64c488805244621
EzeS12 <esaracco12@gmail.com>
2025-12-15 23:13:23 -0300

    estilo carrito html y cambios en carrito js

--- js/carrito.js
+++ js/carrito.js
@@ -1,17 +1,56 @@
-import { agregarAlCarrito } from "./funcionesCarrito";
-import { obtenerCarrito } from "./storage";
-import { actualizarContador } from "./UI";
+import { obtenerCarrito } from "./storage.js";
+import { eliminarProducto, vaciarCarrito } from "./funcionesCarrito.js";
+import { actualizarContador } from "./UI.js";
 
 
 const renderizarCarrito = () => {
-    const carrito = obtenerCarrito()
-    actualizarContador(carrito)
+    const carrito = obtenerCarrito();
+    actualizarContador(carrito);
 
-    const contenedor = document.getElementById("contenedor-carrito")
-    const divAcciones = document.getElementById("acciones-carrito")
+    const contenedor = document.getElementById("contenedor-carrito");
+    const divAcciones = document.getElementById("acciones-carrito");
 
-    contenedor.innerHTML = ""
-    divAcciones.innerHTML = ""
+    contenedor.innerHTML = "";
+    divAcciones.innerHTML = "";
+
+    if (!carrito.length){
+        const mensaje = document.createElement("p");
+        mensaje.classList.add("mensaje-carrito-vacio");
+        mensaje.textContent = "Tu carrito esta vacio";
+
+        contenedor.appendChild(mensaje);
+        return 
+
+    }
+
+    carrito.forEach((producto, indice) =>{
+        const tarjeta = document.createElement("article")
+        tarjeta.classList.add("tarjeta-producto")
+
+        const img = document.createElement("img");
+         img.alt = producto.nombre;
+         img.src = `./Proyecto-Front-End-JS-TT-25/${producto.img}`;
+
+        const precio = document.createElement("p")
+        precio.textContent = `$${producto.precio}`;
+
+        const btnEliminar = document.createElement("button");
+        btnEliminar.classList.add("btn");
+        btnEliminar.classList.add("btn-eliminar-carrito");
+        btnEliminar.textContent = "Eliminar producto"
+
+        btnEliminar.addEventListener("click", () => {
+            eliminarProducto()
+
+
+        })
+
+
+
+
+    }
+
+)
 
 
 }
