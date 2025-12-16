2e27d5c1cb48fd8d7d7d821d10008bfc8720675d
EzeS12 <esaracco12@gmail.com>
2025-12-15 22:28:45 -0300

    funciones carrito cambios

--- js/funcionesCarrito.js
+++ js/funcionesCarrito.js
@@ -1,31 +1,16 @@
-import {guardarCarrito, obtenerCarrito, vaciarCarrito, vaciarCarritoStorage } from "./storage.js";
-import {actualizarContador, mostrarMensaje} from "./UI.js"
+import { guardarCarrito, obtenerCarrito, vaciarCarritoStorage } from "./storage.js";
+import { actualizarContador } from "./UI.js";
 
 export const agregarAlCarrito = (producto) => {
-    const carrito = obtenerCarrito ();
-    carrito.push (producto);
+    const carrito = obtenerCarrito();
+    carrito.push(producto);
     guardarCarrito(carrito);
     actualizarContador(carrito);
-    mostrarMensaje("Producto agregado");
-
-
 };
-
-export const eliminarProducto = (id) => {
-    const carrito = obtenerCarrito ();
-    carrito.splice(id, 1);
-    guardarCarrito(carrito);
-    actualizarContador(carrito);
-    mostrarMensaje("Producto eliminado");
-    
-}
 
 export const vaciarCarrito = () => {
     vaciarCarritoStorage();
     actualizarContador([]);
-    mostrarMensaje ("Carrito vaciado");
-
-    
-}
+};
 
 
